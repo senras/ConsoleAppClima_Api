4cbbfdc083651642cbb55d69f405c2de038e3bf7
Ignacio Senra <isenras@hotmail.com>
2022-02-24 22:01:42 -0300

    Hacer menu inicial

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const { leerInput } = require('./helpers/inquirer');
-
-console.log('Hola mundo');
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
 
 const main = async () => {
-  const texto = await leerInput('Hola: ');
-  console.log(texto);
+  let opt;
+  do {
+    opt = await inquirerMenu();
+    if (opt !== 0) await pausa();
+    // switch (opt) {
+    //   case 1:
+    //     //Buscar ciudad
+    //     break;
+    //   case 2:
+    //     //Historial
+    //     break;
+    //   case 0:
+    //     break;
+    //   default:
+    //     break;
+    // }
+  } while (opt !== 0);
 };
 
 main();
